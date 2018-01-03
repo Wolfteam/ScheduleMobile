3b24d58c5bb124348de7569bbc05f8c5ecb087fd
Efrain Bastidas <mimo4325@gmail.com>
2018-01-02 22:43:52 -0400

    [DI] Agregados algunos scopes y qualifiers

---
+++ app/src/main/java/com/wolfteam20/schedulemobile/di/qualifiers/ActivityContext.java
@@ -0,0 +1,11 @@
+package com.wolfteam20.schedulemobile.di.qualifiers;
+
+import javax.inject.Qualifier;
+
+/**
+ * Created by Efrain Bastidas on 1/2/2018.
+ */
+
+@Qualifier
+public @interface ActivityContext {
+}
---
+++ app/src/main/java/com/wolfteam20/schedulemobile/di/scopes/ActivityScope.java
@@ -0,0 +1,15 @@
+package com.wolfteam20.schedulemobile.di.scopes;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import javax.inject.Scope;
+
+/**
+ * Created by Efrain Bastidas on 1/2/2018.
+ */
+
+@Scope
+@Retention(RetentionPolicy.CLASS)
+public @interface ActivityScope {
+}
