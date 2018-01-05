ba57e042e9fcd1f5254ef474caf1fb65feb4ae60
Efrain.Bastidas <mimo4325@gmail.com>
2018-01-04 20:29:26 -0400

    Algunas utilidades

---
+++ app/src/main/java/com/wolfteam20/schedulemobile/utils/Constants.java
@@ -0,0 +1,9 @@
+package com.wolfteam20.schedulemobile.utils;
+
+/**
+ * Created by Efrain.Bastidas on 1/4/2018.
+ */
+
+public class Constants {
+    public static  final String PREFERENCE_NAME = "schedule_preferences";
+}
---
+++ app/src/main/java/com/wolfteam20/schedulemobile/utils/NetworkUtilities.java
@@ -0,0 +1,23 @@
+package com.wolfteam20.schedulemobile.utils;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+
+import java.io.IOException;
+
+/**
+ * Created by Efrain.Bastidas on 1/4/2018.
+ */
+
+public class NetworkUtilities {
+    public static boolean isNetworkAvailable(Context context) {
+        boolean isConnected = false;
+        try {
+            ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+            isConnected = cm.getActiveNetworkInfo().isConnected();
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+        return isConnected;
+    }
+}
