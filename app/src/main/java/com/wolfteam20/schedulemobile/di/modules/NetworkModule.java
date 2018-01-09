f4fe8895c764a52f21182dac9f3efd81c1961beb
Efrain.Bastidas <mimo4325@gmail.com>
2018-01-09 16:44:47 -0400

    Agregado rxjava al modulo de retrofit. Agregado el token a cada request al cliente http mediante un Interceptor #3

--- app/src/main/java/com/wolfteam20/schedulemobile/di/modules/NetworkModule.java
+++ app/src/main/java/com/wolfteam20/schedulemobile/di/modules/NetworkModule.java
@@ -2,7 +2,7 @@ package com.wolfteam20.schedulemobile.di.modules;
 
 import android.content.Context;
 
-
+import com.wolfteam20.schedulemobile.data.preferences.PreferencesHelperContract;
 import com.wolfteam20.schedulemobile.di.qualifiers.ApplicationContext;
 import com.wolfteam20.schedulemobile.di.scopes.ApplicationScope;
 
@@ -13,13 +13,14 @@ import dagger.Module;
 import dagger.Provides;
 import okhttp3.Cache;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
 import okhttp3.logging.HttpLoggingInterceptor;
 import timber.log.Timber;
 
 /**
  * Created by Efrain.Bastidas on 1/2/2018.
  */
-@Module(includes = ContextModule.class)
+@Module(includes = {ContextModule.class, PreferencesModule.class})
 public class NetworkModule {
 
     @Provides
@@ -37,9 +38,16 @@ public class NetworkModule {
 
     @Provides
     @ApplicationScope
-    OkHttpClient provideOkHttpClient(Cache cache, HttpLoggingInterceptor httpLoggingInterceptor) {
+    OkHttpClient provideOkHttpClient(Cache cache, HttpLoggingInterceptor httpLoggingInterceptor, PreferencesHelperContract prefs) {
         return new OkHttpClient.Builder()
                 .cache(cache)
+                .addInterceptor(chain -> {
+                    String token = prefs.getToken();
+                    Request newRequest  = chain.request().newBuilder()
+                            .addHeader("Authorization", "Bearer " + token)
+                            .build();
+                    return chain.proceed(newRequest);
+                })
                 .addInterceptor(httpLoggingInterceptor)
                 .connectTimeout(60, TimeUnit.SECONDS)
                 .writeTimeout(60, TimeUnit.SECONDS)
@@ -49,14 +57,8 @@ public class NetworkModule {
 
     @Provides
     HttpLoggingInterceptor provideHttpLoggingInterceptor(){
-        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor(new HttpLoggingInterceptor.Logger(){
-            @Override
-            public void log(String message) {
-                Timber.i(message);
-            }
-        });
-        interceptor.setLevel(HttpLoggingInterceptor.Level.HEADERS);
-
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor(message -> Timber.i(message));
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BASIC);
         return interceptor;
     }
 }
--- app/src/main/java/com/wolfteam20/schedulemobile/di/modules/ScheduleServiceModule.java
+++ app/src/main/java/com/wolfteam20/schedulemobile/di/modules/ScheduleServiceModule.java
@@ -10,6 +10,7 @@ import dagger.Module;
 import dagger.Provides;
 import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
@@ -37,6 +38,7 @@ public class ScheduleServiceModule {
         return new Retrofit.Builder()
                 .baseUrl(BuildConfig.URLBaseAPI)
                 .addConverterFactory(GsonConverterFactory.create(gson))
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .client(okHttpClient)
                 .build();
     }
