1f806a0fcfcea2f8ec6a639d0e619ae30d550a6e
Efrain.Bastidas <mimo4325@gmail.com>
2018-02-08 23:50:30 -0400

    Agregados metodos para remover/obtener items de AulasListAdapter

--- app/src/main/java/com/wolfteam20/schedulemobile/ui/adapters/AulasListAdapter.kt
+++ app/src/main/java/com/wolfteam20/schedulemobile/ui/adapters/AulasListAdapter.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import com.wolfteam20.schedulemobile.R
 import com.wolfteam20.schedulemobile.data.network.models.AulaDetailsDTO
+import com.wolfteam20.schedulemobile.data.network.models.DisponibilidadDTO
 import com.wolfteam20.schedulemobile.ui.editardb.EditarDBClickListenerContract
 import kotlinx.android.synthetic.main.aulas_fragment_list_item.view.*
 
@@ -43,9 +44,67 @@ class AulasListAdapter(clickListener: EditarDBClickListenerContract) :
         }
     }
 
+    /**
+     * Obtiene una lista de disponibilidades en las posiciones indicadas
+     * por [itemsPosition]
+     */
+    fun getItems(itemsPosition: ArrayList<Int>): MutableList<AulaDetailsDTO> {
+        return itemsPosition.map { mAulasList[it] }.toMutableList()
+    }
+
     fun setItems(aulas: MutableList<AulaDetailsDTO>) {
         mAulasList = aulas
         notifyDataSetChanged()
+    }
+
+    /**
+     * Remueve un item de la lista en la [position] indicada y
+     * notifica  de los cambios al adapter
+     */
+    private fun removeItem(position: Int) {
+        mAulasList.removeAt(position)
+        notifyItemRemoved(position)
+    }
+
+    /**
+     * Remueve una o varias aulas en las [positions] indicadas
+     * y limpia los elementos seleccionados
+     */
+    fun removeItems(positions: ArrayList<Int>) {
+        //Esto para evitar que se queden items seleccionados
+        clearSelection()
+        positions.sortDescending()
+        while (!positions.isEmpty()) {
+            if (positions.size == 1) {
+                removeItem(positions[0])
+                positions.removeAt(0)
+            } else {
+                var count = 1
+                //Este while sirve para casos donde seleccionas multiples items posiciones seguidas
+                //ee.g(5,4,3)
+                while (positions.size > count && positions[count] == positions[count - 1] - 1) {
+                    ++count
+                }
+                if (count == 1)
+                    removeItem(positions[0])
+                else
+                    removeRange(positions[count - 1], count)
+
+                for (i in 0 until count)
+                    positions.removeAt(0)
+            }
+        }
+    }
+
+    /**
+     * Remueve un rango de aulas partiendo de  [positionStart] hasta [itemCount]
+     * y notifica de los cambios al adapter
+     */
+    private fun removeRange(positionStart: Int, itemCount: Int) {
+        for (i in 0 until itemCount) {
+            mAulasList.removeAt(positionStart)
+        }
+        notifyItemRangeRemoved(positionStart, itemCount)
     }
 
     class AulasListViewHolder(root: View, clickListener: EditarDBClickListenerContract) :
